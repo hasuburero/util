4bc2e15b293b16f4127c1cdde09ca12efcd73db5
hasuburero <hasuburero0802@gmail.com>
2024-12-17 14:12:35 +0900

    add time util

--- times/times.go
+++ times/times.go
@@ -1,23 +1,41 @@
 package times
 
 import (
-  "fmt"
-  "strconv"
-  "strings"
-  "time"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
 )
 
-type TimeStamp struct{
-  Timer time.Time
-  Timestamp string
-  Hour int
-  Min int
-  Sec int
-  Millis int
+type TimeStamp struct {
+	Timer     time.Time
+	Timestamp string // formatted with time.StampMilli
+	Hour      int    // hour part of timestamp
+	Min       int    // min part of timestamp
+	Sec       int    // sec part of timestamp
+	Ms        int    // ms part of timestamp
+	Millis    int    // total milli seconds
 }
 
-func (self *TimeStamp)getTime(){
-  self.Timer = time.Now()
-  self.Timestamp = self.Timer.Format(time.StampMilli)
-  buf := strings.
+func (self *TimeStamp) Getms() {
+	self.Millis = self.Ms + 1000*self.Sec + 60000*self.Min + 3600000*self.Hour
+}
+
+func (self *TimeStamp) GetTime() {
+	self.Timer = time.Now()
+	self.Timestamp = self.Timer.Format(time.StampMilli)
+	buf := strings.Split(self.Timestamp, " ")
+	for i, ctx := range buf {
+		if ctx == "" {
+			buf = append(buf[:i], buf[i+1:]...)
+		}
+	}
+	tbuf := buf[2]
+	buf = strings.Split(tbuf, ":")
+	self.Hour, _ = strconv.Atoi(string(buf[0]))
+	self.Min, _ = strconv.Atoi(string(buf[1]))
+	tbuf = buf[2]
+	buf = strings.Split(tbuf, ".")
+	self.Sec, _ = strconv.Atoi(string(buf[0]))
+	self.Ms, _ = strconv.Atoi(string(buf[1]))
 }

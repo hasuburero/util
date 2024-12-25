a64f66bf68326cf37999389fc00d2f9416c14e78
hasuburero <hasuburero0802@gmail.com>
2024-12-25 09:41:29 +0900

    add byte util

---
+++ byte/byte.go
@@ -0,0 +1,69 @@
+package byte
+
+import (
+	"errors"
+	"unsafe"
+)
+
+const (
+	shift_byte = 8
+	bytesize   = 256
+)
+
+func ByteSize(arg uint) int {
+	var i int = 0
+	for ; i < 4; i++ {
+		arg = arg / bytesize
+		if arg == 0 {
+			break
+		}
+	}
+	return i + 1
+}
+
+func Byte2Int(arg1 []byte, arg2 int) (int, error) {
+	var int_size int = int(unsafe.Sizeof(int(0)))
+	if arg2 > int_size || arg2 <= 0 {
+		err := errors.New("arg2 is out of size int")
+		return 0, err
+	} else if len(arg1) > int_size {
+		err := errors.New("size of arg1 is out of size int")
+		return 0, err
+	}
+	var result int = 0
+	for i := range arg2 {
+		result += (int(arg1[i]) << (shift_byte * (arg2 - 1 - i)))
+	}
+
+	return result, nil
+}
+
+func Int2Byte(arg1 int, arg2 int) ([]byte, error) {
+	var int_size int = int(unsafe.Sizeof(int(0)))
+	if arg2 > int_size || arg2 <= 0 {
+		err := errors.New("arg2 is out of size int")
+		return nil, err
+	}
+	result := make([]byte, arg2)
+	for i := range arg2 {
+		result[arg2-1-i] = byte(arg1 & 0xff)
+		arg1 = arg1 >> shift_byte
+	}
+
+	return result, nil
+}
+
+func Bytecmp(arg1, arg2 []byte) bool {
+	len1 := len(arg1)
+	len2 := len(arg2)
+	if len1 != len2 {
+		return false
+	}
+	for i := 0; i < len1; i++ {
+		if arg1[i] != arg2[i] {
+			return false
+		}
+	}
+
+	return true
+}
